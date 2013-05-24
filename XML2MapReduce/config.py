08f3ad658dd6b02dd2823428f18f953441b0033c
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2013-05-24 20:24:43 +0000

    Removing some of the global variables from config.
    The input path, the output path, and ... are not config options. It is easier to understand the code and maintain it if these values are passsed to the functions as parameters.
    
    
    git-svn-id: http://ysmart.googlecode.com/svn/branches/pure-python@257 1d6e5d1b-7b3d-7f63-4173-3946eeb354c0

--- XML2MapReduce/config.py
+++ XML2MapReduce/config.py
@@ -23,10 +23,3 @@ advanced_agg = False
 ### compiling options
 compile_jar = False
 exec_jar = False
-
-### execution parameters
-output_path = None
-input_path = None
-queryname = None
-scriptname = None
-
