eb47417d388d2853284e82626b445b9d9e57439d
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2013-08-09 09:48:11 -0400

    Code cleanup

--- tests/ysmart/test/testystree.py
+++ tests/ysmart/test/testystree.py
@@ -21,21 +21,25 @@ class Test(BaseTestCase):
 
     def setUp(self):
         homedir = os.getenv('HOME')
-        print homedir
-        self.spark_home = os.getenv('SPARK_HOME', os.path.join(homedir, 'workspace/spark'))
+        default_spark_dir = os.path.join(homedir, 'workspace/spark')
+        self.spark_home = os.getenv('SPARK_HOME', default_spark_dir)
         self.dest_dir = os.path.join(self.spark_home, 'examples/src/main/scala/spark/examples')
 
     def tearDown(self):
         pass
 
 
-    def test_1(self):
+    def test_select_nation(self):
         self._test_scaffold('tests/ysmart/test/unit_tests/tpch_select_nation.sql'
                                   , 'tests/ysmart/test/tpch_test/tpch.schema')
 
-    def test_ssb1_1(self):
-        self._test_scaffold('tests/ysmart/test/unit_tests/tpch_select_nation.sql'
+    def test_join(self):
+        self._test_scaffold('tests/ysmart/test/unit_tests/tpch_join.sql'
                                   , 'tests/ysmart/test/tpch_test/tpch.schema')
+
+    def test_ssb1_1(self):
+        self._test_scaffold('tests/ysmart/test/ssb_test/q1_1.sql'
+                                  , 'tests/ysmart/test/ssb_test/ssb.schema')
 
     def _test_scaffold(self, input_file_path, schema_file_path):
 #         config.turn_on_correlation = True
@@ -47,12 +51,12 @@ class Test(BaseTestCase):
         tree_node = ystree.ysmart_tree_gen(xml_str, schema_str)
         tree_node = correlation.ysmart_correlation(tree_node)
         
-        file_name, file_extionsion = os.path.splitext(os.path.basename(input_file_path))
-        self.assertEqual(file_extionsion, '.sql', '{query_file} does not end with .sql'.format(query_file=input_file_path))
+        file_name, ___ = os.path.splitext(os.path.basename(input_file_path))
         job_name = file_name.title()
         dest_file_path = os.path.join(self.dest_dir, job_name + '.scala')
-        code = translator.spark_code(tree_node, job_name)
+        code = translator.spark_code(tree_node, job_name, self.spark_home)
         with open(dest_file_path, 'w') as job_file:
+            print(os.path.abspath(dest_file_path))
             job_file.write(code)
             job_file.flush()
 
